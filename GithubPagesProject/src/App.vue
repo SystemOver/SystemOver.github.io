961437098ed95e78d9aaceb80f5e5ccb474b1a5e
SystemOver <tassev.stefan@gmail.com>
2025-12-29 17:26:18 +0100

    removed scaleup because it causes wierd scale issues

--- GithubPagesProject/src/App.vue
+++ GithubPagesProject/src/App.vue
@@ -29,41 +29,33 @@ const items = ref([
 </script>
 
 <template>
-  <div class="app-scale">
-    <!--Navbar-->
-    <div class="glass-navbar sticky top-0 left-0 right-0 z-50">
-      <Menubar :model="items">
-        <template #start>
-          <h1 class="Title" @click="$router.push('/')">The System Page</h1>
-        </template>
-        <template #item="{ item, props, hasSubmenu }">
-          <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
-            <a v-ripple :href="href" v-bind="props.action" @click="navigate">
-              <span :class="item.icon" />
-              <span>{{ item.label }}</span>
-            </a>
-          </router-link>
-          <a v-else v-ripple :href="item.url" :target="item.target" v-bind="props.action">
+  <!--Navbar-->
+  <div class="glass-navbar sticky top-0 left-0 right-0 z-50">
+    <Menubar :model="items">
+      <template #start>
+        <h1 class="Title" @click="$router.push('/')">The System Page</h1>
+      </template>
+      <template #item="{ item, props, hasSubmenu }">
+        <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
+          <a v-ripple :href="href" v-bind="props.action" @click="navigate">
             <span :class="item.icon" />
             <span>{{ item.label }}</span>
-            <span v-if="hasSubmenu" class="pi pi-fw pi-angle-down" />
           </a>
-        </template>
-      </Menubar>
-    </div>
-
-    <router-view />
+        </router-link>
+        <a v-else v-ripple :href="item.url" :target="item.target" v-bind="props.action">
+          <span :class="item.icon" />
+          <span>{{ item.label }}</span>
+          <span v-if="hasSubmenu" class="pi pi-fw pi-angle-down" />
+        </a>
+      </template>
+    </Menubar>
   </div>
+
+  <router-view />
 </template>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Nabla&family=Pacifico&family=Passero+One&family=Smooch+Sans:wght@100..900&display=swap');
-
-.app-scale {
-  transform: scale(1.2);      /* 130% */
-  transform-origin: 0 0;      /* keep it anchored to top-left */
-  width: calc(100% / 1.2);    /* prevent horizontal overflow */
-}
 
 .glass-navbar {
   backdrop-filter: blur(12px);
