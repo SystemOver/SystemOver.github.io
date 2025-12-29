2f5039be7e474dbdf5eac9348b7b4cc63f55ffad
SystemOver <tassev.stefan@gmail.com>
2025-12-29 17:23:45 +0100

    added 1.2 scaleup

--- GithubPagesProject/src/pages/Homepage.vue
+++ GithubPagesProject/src/pages/Homepage.vue
@@ -8,8 +8,17 @@ import { Card, Divider } from 'primevue'
       <Card>
         <template #content>
           <p class="justify-self-center">welcome to:</p>
-          <h1 class="text-6xl justify-self-center">The System Page</h1>
+          <h1 class="text-6xl justify-self-center Title mb-8 mt-2">The System Page</h1>
           <Divider class="my-credits-divider"/>
+        <div>
+            <img src="../img/pogchamp.png" alt="testimage" class="float-right max-w-1/3">
+            <div class="clear-left">
+                <h1 class="text-3xl Title">What is this?</h1>
+                <p>Maybe we can live without libraries, people like you and me. Maybe. Sure, we're too old to change the world, but what about that kid, sitting down, opening a book, right now, in a branch at the local library and finding drawings of pee-pees and wee-wees on the Cat in the Hat and the Five Chinese Brothers? Doesn't HE deserve better? Look. If you think this is about overdue fines and missing books, you'd better think again. This is about that kid's right to read a book without getting his mind warped! Or: maybe that turns you on, Seinfeld; maybe that's how y'get your kicks. You and your good-time buddies.</p>
+            </div>
+
+        </div>
+        
         </template>
       </Card>
     </div>
